a1bbfdc02b7e734fa8a11787635eff5737005ac1
Chidi Orji <orjichidi95@gmail.com>
2019-01-18 23:27:24 +0100

    feat: Show url input form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 
 
 class Urls extends Component {
+
+    state = {urls: []}
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const url = e.target.originalUrl.value.trim()
+        console.log(url)
+    }
     render() {
         return (
-            <h1>Url shortener</h1>
+            <div className="container app">
+                <header>
+                    <h1>Url Shortener</h1>
+                    <form 
+                        className="new-url"
+                        onSubmit={this.handleSubmit}>
+                        <input type="url" name="originalUrl" placeholder="Enter url to shorten."/>
+                    </form>
+                </header>
+            </div>
         )
     }
 };
