9db961078572225ddcdb34d13b6c551abfd7601b
Chidi Orji <orjichidi95@gmail.com>
2019-01-19 01:38:11 +0100

    refactor: Make into function component

--- imports/ui/Url.jsx
+++ imports/ui/Url.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-export default class Url extends Component {
-    render() {
-
-        const { url } = this.props
-        return (
-            <li><a href={url.url} target="_blank">{url.short_url}</a></li>
-        )
-    }
+function Url(props) {
+    const { url } = props
+    return (
+        <li><a href={url.url} target="_blank">{url.short_url}</a></li>
+    )
 }
+
+export default Url
