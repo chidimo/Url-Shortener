e25c999220a3ba3c814548688a1163d7608af5d7
Chidi Orji <orjichidi95@gmail.com>
2019-01-19 05:27:20 +0100

    html: Change message display

--- imports/ui/Statistics.jsx
+++ imports/ui/Statistics.jsx
@@ -6,10 +6,7 @@ class Statistics extends Component {
     render() {
         const { Statistics } = this.props
         return (
-            <React.Fragment>
-                <p>Total urls: {this.props.number_of_urls}</p>
-                <p>Total domains: {this.props.number_domains}</p>
-            </React.Fragment>
+            <p>Stats: shortened {this.props.number_of_urls} urls from {this.props.number_domains} domains</p>
         )
     }
 }
