d1a493594fd2898e8f0491b60c0edf9b7551d1a2
Chidi Orji <orjichidi95@gmail.com>
2019-01-19 02:51:27 +0100

    feat: Save domain as part of url data

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -2,6 +2,8 @@ import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
+import * as utilsAPI from './utilsAPI';
+
 export const Urls = new Mongo.Collection('urls');
 
 
@@ -20,7 +22,11 @@ Meteor.methods({
             throw new Meteor.Error('Url already exists.')
         }
 
-        Urls.insert({url: url, createdAt: new Date()});
+        Urls.insert({
+            url: url, 
+            domain: utilsAPI.getDomain(url),
+            createdAt: new Date()
+        });
 
         const u = Urls.findOne({url: url})
         Urls.update({url: url}, {$set: {short_url: u._id}});
