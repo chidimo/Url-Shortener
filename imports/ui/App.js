cb520af1802e73b0904b4b83b68c509cc1809e8a
Chidi Orji <orjichidi95@gmail.com>
2019-01-19 01:05:51 +0100

    fix: Clear input on url save

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
 import { withTracker } from 'meteor/react-meteor-data';
@@ -13,31 +14,33 @@ class App extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         const url = e.target.originalUrl.value.trim()
-        console.log(url)
         Meteor.call('urls.insert', url)
+        ReactDOM.findDOMNode(this.refs.urlInput).value = ''; // clear form
     }
+
     render() {
         return (
             <div className="container app">
                 <header>
                     <h1>Url Shortener</h1>
-                    <form 
-                        className="new-url"
-                        onSubmit={this.handleSubmit}>
-                        <input type="url" name="originalUrl" placeholder="Enter url to shorten."/>
-                    </form>
                 </header>
+                <form 
+                    onSubmit={this.handleSubmit}>
+                    <input 
+                        className="form-control" 
+                        type="url" name="originalUrl" 
+                        placeholder="Enter url to shorten."
+                        ref="urlInput"
+                    />
+                </form>
             </div>
         )
     }
 };
 
-
 export default withRouter(
     withTracker(() => {
         Meteor.subscribe('urls');
-        console.log("With router")
         return {urls: Urls.find().fetch()}
     })(App)
 );
-
