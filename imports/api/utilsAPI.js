ccdbf495651e2f6b35ccd964e786361bc80e903d
Chidi Orji <orjichidi95@gmail.com>
2019-01-19 02:51:02 +0100

    feat: Extract domain from url string

--- imports/api/utilsAPI.js
+++ imports/api/utilsAPI.js
@@ -1,3 +1,8 @@
 export const copyText = (textToCopy) => {
+    // Prompt user to copy the original url of a shortend url
     window.prompt("Copy to clipboard: Ctrl+C, Enter", textToCopy);
+}
+
+export const getDomain = (url) => {
+    return url.split("//")[1].split("/")[0]
 }
