9ce0532a5ffd93636c354a1603c4916cb5ef8d7d
Chidi Orji <orjichidi95@gmail.com>
2019-01-21 00:41:56 +0100

    tests: Move urls test location

---
+++ tests/Url.tests.js
@@ -0,0 +1,25 @@
+import { Factory } from 'meteor/dburles:factory';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import faker from 'faker';
+import chai from 'chai';
+import { expect } from 'chai';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+import Url from '../imports/ui/Url';
+import * as utilsAPI from '../imports/api/utilsAPI';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('<Url />', () => {
+    it('should render', () => {
+        const u = faker.internet.url()
+        const url = Factory.build('url', {
+            url: u, domain: utilsAPI.getDomain(u)
+        });
+        const item = shallow(<Url url={url} />);
+        chai.assert(item.hasClass('url-item'))
+    });
+});
